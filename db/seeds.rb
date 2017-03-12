9cbe0173e0c813c0bf432c009a4b0d065fb8247f
Walsh Costigan <walshcostigan@gmail.com>
2017-03-12 14:51:31 -0500

    commented out the 2 seeds already in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Animal.create(animal_name: "Pig", url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTkkbcmQ3SlibVstRHrYEKGhk2-Fx9kt8-xYT5Gz7F8YU849xHryg")
-Animal.create(animal_name: "Horse", url: "http://www.horsechannel.com/images/horse-news-article-images/chestnut-horse-autumn_1000.jpg")
+#Animal.create(animal_name: "Pig", url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTkkbcmQ3SlibVstRHrYEKGhk2-Fx9kt8-xYT5Gz7F8YU849xHryg")
+#Animal.create(animal_name: "Horse", url: "http://www.horsechannel.com/images/horse-news-article-images/chestnut-horse-autumn_1000.jpg")
